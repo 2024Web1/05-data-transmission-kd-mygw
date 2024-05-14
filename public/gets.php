20c2b254634b6c1e959ebfb9e68890324a51c3dd
miyagawa <kd1318964@st.kobedenshi.ac.jp>
2024-05-14 11:39:44 +0900

    データ送信

---
+++ public/gets.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scal=1.0">
+    <title>GETメソッドのデータ</title>
+</head>
+
+<body>
+    <h4>0J03028 宮川慶士</h4>
+    GETメソッドで送られてきた複数のデータを受け取る<br>
+    <?php
+    echo "出席番号:" .$_GET['course'] .$_GET['class'] .$_GET['number'] .",氏名:" .$_GET['name'];
+    
+    echo '</br>';
+    var_dump($_GET);
+    ?>
+</body>
+
+</html>
